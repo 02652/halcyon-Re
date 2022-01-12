439c436594f33e46c1b9d6afff8e39064ca134c4
10134019 <izhyi02330232@gmail.com>
2022-01-11 22:19:59 -0500

    se cambio de nombre del tema

--- colors/halcyon.lua
+++
@@ -1,2 +0,0 @@
-package.loaded["halcyon"] = nil
-require "halcyon"
--- lua/halcyon/init.lua
+++
@@ -1,7 +0,0 @@
-local utils = require 'halcyon.utils'
-local theme = require 'halcyon.highlight'
-
-vim.o.termguicolors = true
-vim.g.colors_name = "halcyon-moon"
-
-utils.load(theme)
